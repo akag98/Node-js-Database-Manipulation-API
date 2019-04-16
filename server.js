1483645a39a8053a720df179d73405b934fddbd7
akag98 <38176157+akag98@users.noreply.github.com>
2019-04-16 14:35:35 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+var routes = require('./routes/lookup');
+app.use("/", routes);
+
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
