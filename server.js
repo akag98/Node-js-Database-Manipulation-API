6fe47f7885161ee1dced6c2bfd0f0e62b72e02ee
akag98 <38176157+akag98@users.noreply.github.com>
2019-04-24 21:49:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 var routes = require('./routes/lookup');
 app.use("/", routes);
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
    var host = server.address().address
    var port = server.address().port
    
